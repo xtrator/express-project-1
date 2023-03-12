b66d3984ca1246ab027acbb099c89616a6483f41
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-12 11:27:28 -0500

    create connection to database

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-router.get("/", (req, res) => {
-  res.json({ message: "products" });
+router.get("/", async (req, res) => {
+  try {
+    const categories = await prisma.category.findMany();
+    const products = await prisma.product.findMany();
+    res.json({ categories, products });
+  } catch (error) {
+    res.json({ error: error.message });
+  } finally {
+    await prisma.$disconnect();
+  }
 });
 
 module.exports = router;
