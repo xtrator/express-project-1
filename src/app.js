005eb4255a18e7b8abc37439668990a3cb59d7a1
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-12 18:36:46 -0500

    add cors to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 //http://expressjs.com/en/api.html#express.json
 app.use(express.json());
 //http://expressjs.com/en/5x/api.html#express.urlencoded
