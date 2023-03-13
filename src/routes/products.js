b96c3db27ce90c7dfd6383246d3a23b00369b888
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-13 15:00:08 -0500

    implement route to fetch products by category

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-router.get("/", async (req, res) => {
+router.get("/categories", async (req, res) => {
   try {
-    const products = await prisma.product.findMany();
-    res.json({ products });
+    const categories = await prisma.category.findMany();
+    res.json({ categories });
   } catch (error) {
     res.json({ error: error.message });
   } finally {
@@ -14,10 +14,14 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/categories", async (req, res) => {
+router.get("/:category_id", async (req, res) => {
   try {
-    const categories = await prisma.category.findMany();
-    res.json({ categories });
+    const products = await prisma.product.findMany({
+      where: {
+        category: Number(req.params.category_id),
+      },
+    });
+    res.json({ products });
   } catch (error) {
     res.json({ error: error.message });
   } finally {
